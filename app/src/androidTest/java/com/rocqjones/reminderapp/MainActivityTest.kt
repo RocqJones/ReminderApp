b01c5ef8932506c7fab88c387f156fada7a073c4
RocqJones <jonesmbindyo@gmail.com>
2023-05-22 22:13:49 +0300

    final Android UI tests

--- app/src/androidTest/java/com/rocqjones/reminderapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/rocqjones/reminderapp/MainActivityTest.kt
@@ -1,24 +1,26 @@
 package com.rocqjones.reminderapp
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import com.rocqjones.reminderapp.ui.theme.ReminderAppTheme
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 
 @RunWith(AndroidJUnit4::class)
@@ -31,7 +33,14 @@ class MainActivityTest {
     fun setUp() {
         // Set up your Compose UI here
         composeTestRule.setContent {
-            MainActivity()
+            ReminderAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ListItems()
+                }
+            }
         }
     }
 
@@ -47,78 +56,63 @@ class MainActivityTest {
 
     @Test
     fun testOpenReminderDialog() {
-        composeTestRule.onNodeWithTag("composeCard_0").performClick()
-
-        // Simulate a click on the first ComposeCard
-        //composeTestRule.onNodeWithTag("composeCard").performClick()
+        composeTestRule.onNodeWithTag(
+            "list_items"
+        ).assertExists().onChildren()[0].assertExists().performClick()
 
         // Verify that the ReminderDialog is displayed
-        //composeTestRule.onNodeWithText("Remind me in…").assertExists()
-        onView(withText(R.string.title_reminder))
-            .check(matches(isDisplayed()))
+        composeTestRule.onNodeWithTag("reminderDialog").assertExists()
 
-        // Click on the "5 seconds" schedule
-        onView(withText(R.string.schedule_5_seconds))
-            .perform(click())
+        val schedules = listOf(
+            "5 seconds" to 5000L,
+            "8 minutes" to 8 * 60 * 1000L,
+            "1 day" to 24 * 60 * 60 * 1000L,
+            "1 week" to 7 * 24 * 60 * 60 * 1000L
+        )
 
-        // Wait for 5 seconds
-        Thread.sleep(5000)
+        composeTestRule.onNodeWithTag("reminderDialog")
+            .assertExists()
 
-        // Check if the notification appears
-        onView(withText("Reminder App."))
-            .check(matches(isDisplayed()))
-    }
-
-
-    /*@get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
-    @Before
-    fun setUp() {
-        // Wait for the UI to be visible
-        composeTestRule.setContent {
-            ReminderAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ListItems(modifier = Modifier.testTag("list_items"))
-                }
-            }
+        for ((scheduleText, _) in schedules) {
+            composeTestRule.onNodeWithText(scheduleText)
+                .assertExists()
         }
-    }
 
-    @Test
-    fun testReminderDialogNotificationAndNavigation() {
-        // Click on the first card
-        composeTestRule.onNodeWithTag("list_items").onChildren()[0].performClick()
-
-        // Wait for the ReminderDialog to be displayed
-        onView(withText(R.string.title_reminder))
-            .check(matches(isDisplayed()))
-
-        // Click on the "5 seconds" schedule
-        onView(withText(R.string.schedule_5_seconds))
-            .perform(click())
+        composeTestRule.onNodeWithText("5 seconds").assertExists().performClick()
 
         // Wait for 5 seconds
-        Thread.sleep(5000)
+        Thread.sleep(TimeUnit.SECONDS.toMillis(5))
 
         // Check if the notification appears
-        onView(withText("Reminder App."))
-            .check(matches(isDisplayed()))
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val notificationExists = device.wait(
+            Until.hasObject(By.pkg("com.rocqjones.reminderapp").depth(0)),
+            TimeUnit.SECONDS.toMillis(10)
+        )
+        Assert.assertTrue("Notification should appear", notificationExists != null)
 
-        // Click on the notification
-        onView(withText("Reminder App."))
-            .perform(click())
+        // Click the notification
+        val notification = device.findObject(By.text("Reminder App."))
+        notification?.click()
 
-        // Check if the defined activity is opened
-        composeTestRule.onNodeWithText("Reminder App")
-            .assertIsDisplayed()
-    }*/
+        // Verify that the MainActivity is launched
+        val instrumentation = InstrumentationRegistry.getInstrumentation()
+        instrumentation.waitForIdleSync()
+        val currentActivity = instrumentation.targetContext.packageManager.getLaunchIntentForPackage(
+            instrumentation.targetContext.packageName
+        )?.component?.className
+        Assert.assertEquals(
+            "MainActivity should be launched",
+            MainActivity::class.java.name,
+            currentActivity
+        )
+
+        // Clear the notification
+        val notificationManager = instrumentation.targetContext.getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+        notificationManager.cancel(17)
+    }
 }
 
 
