d770b5a41be638386bc61de56a5c8ca848a4c84f
RocqJones <jonesmbindyo@gmail.com>
2023-10-24 12:59:38 +0300

    agp

--- app/src/main/java/com/rocqjones/reminderapp/MainActivity.kt
+++ app/src/main/java/com/rocqjones/reminderapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.rocqjones.reminderapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
@@ -14,7 +17,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,14 +34,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.rocqjones.reminderapp.data.DataSource
 import com.rocqjones.reminderapp.models.ComposeRandomItem
 import com.rocqjones.reminderapp.ui.dialogs.ReminderDialog
 import com.rocqjones.reminderapp.ui.theme.ReminderAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private var TAG = "MainActivity"
+        const val REQUEST_CODE_NOTIFICATION_PERMISSIONS = 11
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        getNotificationPermissions()
+
         setContent {
             ReminderAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -45,18 +64,81 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun getNotificationPermissions() {
+        try {
+            // Check if the app already has the permissions.
+            val hasAccessNotificationPolicyPermission =
+                checkSelfPermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY) == PackageManager.PERMISSION_GRANTED
+            val hasPostNotificationsPermission =
+                checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+
+            // If the app doesn't have the permissions, request them.
+            when {
+                !hasAccessNotificationPolicyPermission || !hasPostNotificationsPermission -> {
+                    // Request the permissions.
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(
+                            Manifest.permission.ACCESS_NOTIFICATION_POLICY,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ),
+                        REQUEST_CODE_NOTIFICATION_PERMISSIONS
+                    )
+                }
+                else -> {
+                    // proceed
+                    Log.d(TAG, "Notification Permissions : previously granted successfully")
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            // Check if the user granted the permissions.
+            REQUEST_CODE_NOTIFICATION_PERMISSIONS -> {
+                val hasAccessNotificationPolicyPermission =
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                val hasPostNotificationsPermission =
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+
+                // If the user denied the permissions, show a check.
+                when {
+                    !hasAccessNotificationPolicyPermission || !hasPostNotificationsPermission -> {
+                        getNotificationPermissions()
+                    }
+                    else -> {
+                        Log.d(TAG, "Notification Permissions : Granted successfully")
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun ListItems(
     modifier: Modifier = Modifier,
-    data: List<ComposeRandomItem> = DataSource.plants.map { it }  // names: List<String> = List(30) { "$it" }
+    data: List<ComposeRandomItem> = DataSource.plants.map { it },  // names: List<String> = List(30) { "$it" }
 ) {
     /**
      * Let's create a performant lazy list.
      * Note that these are the equivalent component to RecyclerView or ListView from Views in Compose.
      */
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp).testTag("list_items")) {
+    LazyColumn(
+        modifier = modifier
+            .padding(vertical = 4.dp)
+            .testTag("list_items")
+    ) {
         itemsIndexed(items = data.toMutableList()) { index, n ->
             ComposeCard(
                 name = n.name,
@@ -76,9 +158,11 @@ fun ComposeCard(name: String, type: String, description: String, modifier: Modif
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primary
         ),
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp).clickable {
-            dialogState.value = true
-        }
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .clickable {
+                dialogState.value = true
+            }
     ) {
         CardContent(name, type, description)
     }
@@ -113,10 +197,13 @@ fun CardContent(name: String, type: String, description: String) {
             )
     ) {
         Column(
-            modifier = Modifier.weight(1f).padding(12.dp)
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
         ) {
             Text(text = type)
-            Text(text = "$name.",
+            Text(
+                text = "$name.",
                 style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.ExtraBold
                 )
@@ -132,7 +219,9 @@ fun CardContent(name: String, type: String, description: String) {
 
         IconButton(onClick = { expanded.value = !expanded.value }) {
             Icon(
-                painter = if (expanded.value) painterResource(id = R.drawable.baseline_expand_less_24) else painterResource(id = R.drawable.baseline_expand_more_24),
+                painter = if (expanded.value) painterResource(id = R.drawable.baseline_expand_less_24) else painterResource(
+                    id = R.drawable.baseline_expand_more_24
+                ),
                 contentDescription = if (expanded.value) {
                     stringResource(R.string.show_less)
                 } else {
